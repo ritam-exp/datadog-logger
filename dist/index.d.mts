bebdb9d7157822d68d55cc35af399f459593e52e
Ritam Chakraborty <ritam.chakraborty@exprealty.net>
2025-01-11 04:41:08 +0530

    New build

--- dist/index.d.mts
+++ dist/index.d.mts
@@ -2,11 +2,17 @@ declare class Logger {
     private readonly datadogUrl;
     private readonly datadogApiKey;
     private readonly hostname;
+    private readonly environment;
     private readonly sendToDatadog;
     private readonly logMessages;
-    constructor(datadogUrl: string, datadogApiKey: string, hostname: string, sendToDatadog: boolean);
-    log(message: string, tags: string): void;
+    constructor(datadogUrl: string, datadogApiKey: string, hostname: string, environment: string, sendToDatadog: boolean);
+    info(message: string | object, tags?: object | string[]): void;
+    warn(message: string | object, tags?: object | string[]): void;
+    error(message: string | object, tags?: object | string[]): void;
     flush(): Promise<void>;
+    private getFormattedTags;
+    private log;
+    private getFormattedLogMessage;
 }
 
 export { Logger };
--- dist/index.mjs
+++ dist/index.mjs
@@ -1,45 +1,101 @@
 // src/index.ts
 import axios from "axios";
 var Logger = class {
-  constructor(datadogUrl, datadogApiKey, hostname, sendToDatadog) {
+  constructor(datadogUrl, datadogApiKey, hostname, environment, sendToDatadog) {
     this.datadogUrl = datadogUrl;
     this.datadogApiKey = datadogApiKey;
     this.hostname = hostname;
+    this.environment = environment;
     this.sendToDatadog = sendToDatadog;
   }
   logMessages = new Array();
-  log(message, tags) {
-    if (!this.sendToDatadog) {
-      console.log(message);
-      return;
-    }
-    this.logMessages.push({ message, tags });
+  info(message, tags) {
+    this.log(message, "info", tags);
+  }
+  warn(message, tags) {
+    this.log(message, "warn", tags);
+  }
+  error(message, tags) {
+    this.log(message, "error", tags);
   }
   async flush() {
     if (!this.sendToDatadog) return;
-    for (const logMessage of this.logMessages) {
+    const logsToSend = [...this.logMessages];
+    const promises = [];
+    for (const logMessage of logsToSend) {
+      const formattedTags = this.getFormattedTags(logMessage.tags);
       const payload = {
         message: logMessage.message,
         ddsource: "twilio",
-        ddtags: `index:twilio,env:development,service:twilio-serverless,baseUrl:${logMessage.tags}`,
+        ddtags: `index:twilio,env:${this.environment},${formattedTags}`,
         hostname: this.hostname,
-        service: "twilio-service"
+        service: "twilio-service",
+        status: logMessage.level
       };
       const config = {
         headers: {
           "Content-Type": "application/json"
         }
       };
-      try {
-        await axios.post(
-          `${this.datadogUrl}?dd-api-key=${this.datadogApiKey}`,
-          payload,
-          config
-        );
-      } catch (error) {
-        throw new Error(`Unable to send logs to Datadog. Payload: ${JSON.stringify(payload)} Error: ${error}`);
-      }
+      const promise = axios.post(
+        `${this.datadogUrl}?dd-api-key=${this.datadogApiKey}`,
+        payload,
+        config
+      );
+      promises.push(promise);
     }
+    const responses = await Promise.allSettled(promises);
+    for (const response of responses) {
+      this.logMessages.shift();
+    }
+  }
+  getFormattedTags(tags) {
+    if (typeof tags === "object") {
+      return Object.entries(tags).map(
+        ([key, value]) => typeof value === "object" ? `${key}:${JSON.stringify(value)}` : `${key}:${value}`
+      ).join(",");
+    }
+    return tags ?? "";
+  }
+  log(message, level, tags) {
+    if (!message || typeof message === "string" && message.length === 0) {
+      throw new Error("Cannot send empty log");
+    }
+    if (this.sendToDatadog) {
+      this.logMessages.push({ message, level, tags });
+      return;
+    }
+    const formattedLogMessage = this.getFormattedLogMessage(message, level);
+    switch (level) {
+      case "info":
+        console.info(formattedLogMessage);
+        return;
+      case "warn":
+        console.warn(formattedLogMessage);
+        return;
+      case "error":
+        console.error(formattedLogMessage);
+        return;
+      default:
+        return;
+    }
+  }
+  getFormattedLogMessage(message, level) {
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      hour12: false,
+      timeZoneName: "short"
+    };
+    const dateTimeFormatter = new Intl.DateTimeFormat("en-CA", options);
+    const formattedDate = dateTimeFormatter.format(/* @__PURE__ */ new Date());
+    const formatedMessage = typeof message === "object" ? JSON.stringify(message) : message;
+    return `${formattedDate} :: ${level.toUpperCase().padStart(5, " ")} :: ${formatedMessage}`;
   }
 };
 export {
